723d0bef93b20137d3480905a1118049f13516d6
Nikolay Brovkin <bronik004@gmail.com>
2022-08-19 11:24:42 +0300

    feat: added header scroll

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,1 +1,15 @@
 import './index.css';
+
+import logoBig from '../images/logo.svg';
+import logoScroll from '../images/logo-scroll.svg';
+
+const logo = document.querySelector(".logo__img");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY) {
+    logo.style.margin = '12px 0';
+    logo.src = logoScroll;
+  } else {
+    logo.src = logoBig;
+  }
+});
