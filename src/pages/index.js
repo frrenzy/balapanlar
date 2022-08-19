825550e92604bb2f28c5862f1dc89203ac0b0010
Nikolay Brovkin <bronik004@gmail.com>
2022-08-19 13:09:01 +0300

    fix: changed logo name and remove transition

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,12 @@
 import './index.css';
 
 import logoBig from '../images/logo.svg';
-import logoScroll from '../images/logo-scroll.svg';
+import logoScroll from '../images/logo-small.svg';
 
 const logo = document.querySelector(".logo__img");
 
 window.addEventListener("scroll", () => {
-  if (window.scrollY) {
+  if (window.scrollY > 0) {
     logo.style.margin = '12px 0';
     logo.src = logoScroll;
   } else {
