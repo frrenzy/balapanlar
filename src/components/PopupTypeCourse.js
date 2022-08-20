ddd7825ba4393d88e965295376d0f9548bebf108
ivanfonin <ivanfonin@yandex.ru>
2022-08-20 18:39:38 +0300

    feat(popup-course-js): Added Popup.js, PopupTypeCourse.js

---
+++ src/components/PopupTypeCourse.js
@@ -0,0 +1,33 @@
+import { Popup } from './Popup';
+
+export class PopupTypeCourse extends Popup {
+  constructor(selector) {
+    super(selector);
+  }
+
+  _getCourseContent(id) {
+    const courseContent = document
+      .querySelector(`#course-id-${id}`)
+      .content
+      .querySelector('.popup__content')
+      .cloneNode(true);
+    
+    this._element.querySelector('.popup__buttons a').setAttribute('href', document.querySelector(`#course-id-${id}`).dataset.link);
+
+    return courseContent;
+  }
+
+  open(courseId) {
+    super.open();
+    let courseContent = this._getCourseContent(courseId);
+    this._element.querySelector('.popup__container').prepend(courseContent);
+  }
+
+  close() {
+    super.close();
+    setTimeout(() => {
+      this._element.querySelector('.popup__content').remove();
+      this._element.querySelector('.popup__buttons a').setAttribute('href', '');
+    }, 250);
+  }
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,11 @@
 import './index.css';
 
+import { PopupTypeCourse } from '../components/PopupTypeCourse';
+
 import logoBig from '../images/logo.svg';
 import logoScroll from '../images/logo-small.svg';
 
+const coursePopup = new PopupTypeCourse('.popup_type_course');
 const logo = document.querySelector('.logo__img');
 const page = document.querySelector('.page');
 
@@ -16,4 +19,12 @@ window.addEventListener('scroll', () => {
     page.style.background = 'linear-gradient(180deg, #ffffff 180px, #eaf0ff 0)';
     logo.style.margin = '0';
   }
+});
+
+window.addEventListener('click', (evt) => {
+  if (!evt.target.hasAttribute('data-course-id')) {
+    return;
+  }
+
+  coursePopup.open(evt.target.dataset.courseId);
 });
