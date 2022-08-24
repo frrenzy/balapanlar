468005d7e4f476ddbdc2b1d38153766756068dce
Nikolay Brovkin <bronik004@gmail.com>
2022-08-25 00:04:17 +0600

    feat: added tablet and fix review bugs

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,12 +5,12 @@ import logoScroll from '../images/logo-small.svg';
 
 const logo = document.querySelector('.logo__img');
 const page = document.querySelector('.page');
-const burgerButton = document.querySelector(".header__burger");
-const navigation = document.querySelector(".header__nav");
+const burgerButton = document.querySelector('.header__burger');
+const navigation = document.querySelector('.header__nav');
 
 burgerButton.addEventListener('click', () => {
   navigation.classList.toggle('header__nav_opened');
-  burgerButton.classList.toggle('header__nav_opened');
+  burgerButton.classList.toggle('header__burger_opened');
 });
 
 window.addEventListener('scroll', () => {
