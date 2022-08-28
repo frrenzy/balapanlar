b24eb42e231162964d185936088936992723ad4a
Ivan Sizov <vsizov63@gmail.com>
2022-08-28 19:54:05 +0300

    fix: fix burger not opening on click

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,3 +1,5 @@
+import { navigation, burgerButton } from "./constants";
+
 export const toggleBurger = () => {
   navigation.classList.toggle('header__nav_opened');
   burgerButton.classList.toggle('header__burger_opened');
